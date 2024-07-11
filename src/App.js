8ebf84224f2e94ab419f69afe146621230eed196
bhanutejaindla <bhanutejaindla123@gmail.com>
2024-07-12 00:31:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './index.css';
+import Navbar from './components/Navbar/Navbar.jsx';
+import {Routes,Route} from 'react-router-dom';
+import Home from './pages/Home/Home.jsx';
+import Coin from './pages/Coin/Coin.jsx';
+import Footer from './components/Footer/Footer.jsx';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/coin/:coinId' element={<Coin />} />
+       </Routes>
+       <Footer />
     </div>
   );
 }
---
+++ src/context/CoinContext.jsx
@@ -0,0 +1,95 @@
+// import React,{ createContext, useEffect, useState } from "react";
+
+// export const CoinContext=createContext();
+
+// const CoinContextProvider=(props)=>{
+    
+//     const [allCoin,setAllCoin]=useState([]);
+//     const [currency,setCurrency]=useState({
+//         name:"usd",
+//         symbol:"$"
+//     });
+    
+//     const fetchAllcoin =async ()=>{
+//         const options = {
+//             method: 'GET',
+//             headers: {
+//               accept: 'application/json',
+//               'x-cg-demo-api-key': 'CG-fuQy2nY2tmZNz6hkpJsDYVw7 '
+//             }
+//           };
+          
+//           fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency={currency.name}`, options)
+//             .then(response => response.json())
+//             .then(response => setAllCoin(response))
+//             .catch(err => console.error(err));
+//     }
+
+// useEffect(()=>{
+//     fetchAllcoin(); 
+// },[currency])
+//     const contextValue ={
+//             allCoin,currency,setCurrency
+//     }
+//     return(
+//         <CoinContext.Provider value={contextValue}>
+//             {props.children}
+//         </CoinContext.Provider>
+//     )
+// }
+
+// export default CoinContextProvider;
+
+import React, { createContext, useEffect, useState } from "react";
+
+export const CoinContext = createContext();
+
+const CoinContextProvider = (props) => {
+  const [allCoin, setAllCoin] = useState([]);
+  const [currency, setCurrency] = useState({
+    name: "usd",
+    symbol: "$",
+  });
+
+  const fetchAllcoin = async () => {
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        'x-cg-demo-api-key': 'CG-fuQy2nY2tmZNz6hkpJsDYVw7',
+      },
+    };
+
+    try {
+      const response = await fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency.name}`, options);
+      const data = await response.json();
+      if (Array.isArray(data)) {
+        setAllCoin(data);
+      } else {
+        setAllCoin([]);
+        console.error('Data fetched is not an array:', data);
+      }
+    } catch (err) {
+      console.error('Error fetching coin data:', err);
+      setAllCoin([]);
+    }
+  };
+
+  useEffect(() => {
+    fetchAllcoin();
+  }, [currency]);
+
+  const contextValue = {
+    allCoin,
+    currency,
+    setCurrency,
+  };
+
+  return (
+    <CoinContext.Provider value={contextValue}>
+      {props.children}
+    </CoinContext.Provider>
+  );
+};
+
+export default CoinContextProvider;
